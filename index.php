db8d154ac0340c26b7d668ca142439d24557fd71
Daryle <daryle.urrea@hotmail.com>
2019-10-27 21:50:44 -0400

    Third commit trial

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Daryle: Test2</title>
+        <title>Daryle: Test3</title>
     </head>
     <body>
         <?php
