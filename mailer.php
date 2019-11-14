cf6ec453a793a4d3666847f4ae00e925eccf0d78
Daryle <daryle.urrea@hotmail.com>
2019-11-14 15:33:51 -0500

    change subject of mailer to include sender's email

--- mailer.php
+++ mailer.php
@@ -55,7 +55,7 @@ try {
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = $subject;
+    $mail->Subject = $subject . "From: " . $email;
     $mail->Body    = $message;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
